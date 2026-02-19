3f3f22fd7bef01014444330b0c0df0e1b1837fa5
Ollie Hockey <oh123@ic.ac.uk>
2026-02-19 13:38:02 +0000

    Add UART demo for keypad functionality
    Implement UART communication for keypad input.

--- keypad_URAT.s
+++ keypad_URAT.s
@@ -1,1 +1,20 @@
-; enter UART code for the LCD screen here
+#include <xc.inc>
+
+extrn   KeyPad_Setup, KeyPad_GetASCII, KeyPad_WaitRelease
+extrn   UART_Setup, UART_Transmit_Byte
+
+global  KeyPad_UART_Demo
+
+psect   keypad_uart_code, class=CODE
+
+KeyPad_UART_Demo:
+        call    KeyPad_Setup
+        call    UART_Setup
+
+uart_loop:
+        call    KeyPad_GetASCII     ; W = ASCII, or 0 if none
+        bz      uart_loop
+
+        call    UART_Transmit_Byte
+        call    KeyPad_WaitRelease
+        bra     uart_loop
