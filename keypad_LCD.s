fd8c2a5f4398c82f243f934855828cb646213fba
Ollie Hockey <oh123@ic.ac.uk>
2026-02-19 13:37:23 +0000

    Implement keypad LCD demo functionality

--- keypad_LCD.s
+++ keypad_LCD.s
@@ -1,1 +1,20 @@
-; enter keypad LCD code here
+#include <xc.inc>
+
+extrn   KeyPad_Setup, KeyPad_GetASCII, KeyPad_WaitRelease
+extrn   LCD_Setup, LCD_Write_Char
+
+global  KeyPad_LCD_Demo
+
+psect   keypad_lcd_code, class=CODE
+
+KeyPad_LCD_Demo:
+        call    KeyPad_Setup
+        call    LCD_Setup
+
+lcd_loop:
+        call    KeyPad_GetASCII     ; W = ASCII, or 0 if none
+        bz      lcd_loop
+
+        call    LCD_Write_Char      ; print one char
+        call    KeyPad_WaitRelease  ; prevent repeat while held
+        bra     lcd_loop
