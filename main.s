4b07bef97ceb3270a39d105003d7cb7d1ccab494
oh123 <oh123@skbl-418-007.ph.ic.ac.uk>
2026-02-19 12:05:54 +0000

    new branch

--- main.s
+++ main.s
@@ -1,1 +1,21 @@
-hello
+#include <xc.inc>
+
+extrn   KeyPad_Setup, KeyPad_GetValue
+
+psect   code, abs
+rst:    org 0x0000
+        goto start
+
+psect   code
+start:
+        clrf    TRISD, A
+        clrf    LATD, A
+
+        call    KeyPad_Setup
+
+loop:
+        call    KeyPad_GetValue     ; W = 0..15 or 0xFF
+        movwf   LATD, A
+        bra     loop
+
+        end rst
