4f01ab04719cec06cbbe08790a80fa7bd492bea4
Ollie Hockey <oh123@ic.ac.uk>
2026-02-20 11:24:40 +0000

    Refactor main.s for LCD service integration

--- main.s
+++ main.s
@@ -1,21 +1,24 @@
+;========================
+; main.s
+;========================
 #include <xc.inc>
 
-extrn   KeyPad_Setup, KeyPad_GetValue
+extrn   KeyPad_Setup, KeyPad_LCD_Service
+extrn   LCD_Setup, LCD_clear_display, LCD_first_line
 
 psect   code, abs
-rst:    org 0x0000
-        goto start
+rst:    org     0x0000
+        goto    start
 
 psect   code
 start:
-        clrf    TRISD, A
-        clrf    LATD, A
-
+        call    LCD_Setup
+        call    LCD_clear_display
+        call    LCD_first_line
         call    KeyPad_Setup
 
-loop:
-        call    KeyPad_GetValue     ; W = 0..15 or 0xFF
-        movwf   LATD, A
-        bra     loop
+main_loop:
+        call    KeyPad_LCD_Service
+        bra     main_loop
 
-        end rst
+        end     rst
