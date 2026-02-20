2be72dfeb2403f93b4f435a43686286dc506fc2e
Ollie Hockey <oh123@ic.ac.uk>
2026-02-20 11:24:24 +0000

    Implement keypad reading and LCD service functions - code cleaned and formatted by GPT for efficiency
    Added KeyPad_Read and KeyPad_LCD_Service functions for keypad handling and LCD interaction.

--- keypad.s
+++ keypad.s
@@ -2,14 +2,16 @@
 
 global  KeyPad_Setup, KeyPad_ReadColumns
 global  KeyPad_GetValue
-    
+global  KeyPad_Read
+global  KeyPad_LCD_Service
+
 psect   udata_acs
 column_pressed: ds 1
-keypad_value: ds   1
+keypad_value:   ds 1
+key_char:       ds 1        ; used by LCD service routine
 
 psect   keypad_code, class=CODE
 
-   
 KeyPad_Setup:
         ; pull-ups on for PORTE
         movlb   0x0F
@@ -23,29 +25,24 @@ KeyPad_Setup:
         ; rows idle high
         movlw   0xF0
         movwf   LATE, A
-
         return
 
 
-
 KeyPad_ReadColumns:
-	; reads 0-3 to see what column is pressed
-	; if no column presed return 0-3 as high
-	; returns low bit on column pressed from 0-3
         movf    PORTE, W, A
         andlw   0x0F
         return
 
+
 ; ------------------------------------------------------------
 ; KeyPad_GetValue
-; Returns in W:
-;   0..15  = key index in binary
+; Returns:
+;   0..15  = key index
 ;   0xFF   = no key
-;
 ; ------------------------------------------------------------
 KeyPad_GetValue:
 
-        ; ---- row 0: RE4 low ----
+        ; ---- row 0 ----
         movlw   0xF0
         movwf   LATE, A
         bcf     LATE, 4, A
@@ -57,7 +54,7 @@ KeyPad_GetValue:
         cpfseq  column_pressed, A
         bra     row0_pressed
 
-        ; ---- row 1: RE5 low ----
+        ; ---- row 1 ----
         movlw   0xF0
         movwf   LATE, A
         bcf     LATE, 5, A
@@ -69,7 +66,7 @@ KeyPad_GetValue:
         cpfseq  column_pressed, A
         bra     row1_pressed
 
-        ; ---- row 2: RE6 low ----
+        ; ---- row 2 ----
         movlw   0xF0
         movwf   LATE, A
         bcf     LATE, 6, A
@@ -81,7 +78,7 @@ KeyPad_GetValue:
         cpfseq  column_pressed, A
         bra     row2_pressed
 
-        ; ---- row 3: RE7 low ----
+        ; ---- row 3 ----
         movlw   0xF0
         movwf   LATE, A
         bcf     LATE, 7, A
@@ -93,49 +90,200 @@ KeyPad_GetValue:
         cpfseq  column_pressed, A
         bra     row3_pressed
 
-        ; none pressed
         movlw   0xFF
         return
 
 
-; each row starts at 0, 4, 8, 12
-	
-row0_pressed:
-        movlw   0
-        bra     decode_col
-
-row1_pressed:
-        movlw   4
-        bra     decode_col
-
-row2_pressed:
-        movlw   8
-        bra     decode_col
-
-row3_pressed:
-        movlw   12
-        bra	decode_col
+row0_pressed:  movlw   0   ; base index
+                bra     decode_col
+row1_pressed:  movlw   4
+                bra     decode_col
+row2_pressed:  movlw   8
+                bra     decode_col
+row3_pressed:  movlw   12
+                bra     decode_col
 
 
 decode_col:
-        ; column_pressed holds values D0-D3
-	; btfss checks bit position in second argument and skips if 1
-	; adds on the extra 1 and cheks gain
+        btfsc   column_pressed, 0, A
+        bra     col1
+        return
 
-	movwf	keypad_value, A
-        btfss   column_pressed, 0, A
+col1:   addlw   1
+        btfsc   column_pressed, 1, A
+        bra     col2
         return
-        addlw   1
-        
-	movwf	keypad_value, A
-	btfss   column_pressed, 1, A
+
+col2:   addlw   1
+        btfsc   column_pressed, 2, A
+        bra     col3
         return
-        addlw   1
-        
-	movwf	keypad_value, A
-	btfss   column_pressed, 2, A
-	return
-        addlw   1
-	
-	movwf	keypad_value, A
-	return
+
+col3:   addlw   1
+        return
+
+
+; ------------------------------------------------------------
+; Convert key index → ASCII
+; ------------------------------------------------------------
+KeyPad_Read:
+        call    KeyPad_GetValue
+        movwf   keypad_value, A
+
+        movlw   0xFF
+        cpfseq  keypad_value, A
+        bra     kp0
+        retlw   0
+
+kp0:    movlw   0x00
+        cpfseq  keypad_value, A
+        bra     kp1
+        retlw   '1'
+kp1:    movlw   0x01
+        cpfseq  keypad_value, A
+        bra     kp2
+        retlw   '2'
+kp2:    movlw   0x02
+        cpfseq  keypad_value, A
+        bra     kp3
+        retlw   '3'
+kp3:    movlw   0x03
+        cpfseq  keypad_value, A
+        bra     kp4
+        retlw   'F'
+kp4:    movlw   0x04
+        cpfseq  keypad_value, A
+        bra     kp5
+        retlw   '4'
+kp5:    movlw   0x05
+        cpfseq  keypad_value, A
+        bra     kp6
+        retlw   '5'
+kp6:    movlw   0x06
+        cpfseq  keypad_value, A
+        bra     kp7
+        retlw   '6'
+kp7:    movlw   0x07
+        cpfseq  keypad_value, A
+        bra     kp8
+        retlw   'E'
+kp8:    movlw   0x08
+        cpfseq  keypad_value, A
+        bra     kp9
+        retlw   '7'
+kp9:    movlw   0x09
+        cpfseq  keypad_value, A
+        bra     kpA
+        retlw   '8'
+kpA:    movlw   0x0A
+        cpfseq  keypad_value, A
+        bra     kpB
+        retlw   '9'
+kpB:    movlw   0x0B
+        cpfseq  keypad_value, A
+        bra     kpC
+        retlw   'D'
+kpC:    movlw   0x0C
+        cpfseq  keypad_value, A
+        bra     kpD
+        retlw   'A'
+kpD:    movlw   0x0D
+        cpfseq  keypad_value, A
+        bra     kpE
+        retlw   '0'
+kpE:    movlw   0x0E
+        cpfseq  keypad_value, A
+        bra     kpF
+        retlw   'B'
+kpF:    movlw   0x0F
+        cpfseq  keypad_value, A
+        bra     kp_unknown
+        retlw   'C'
+
+kp_unknown:
+        retlw   0
+
+
+; ============================================================
+; KeyPad_LCD_Service
+; ============================================================
+
+extrn   LCD_first_line, LCD_second_line
+extrn   LCD_shift_right_crsr, LCD_shift_left_crsr
+extrn   LCD_clear_display, LCD_Write_Char
+extrn   LCD_delay_ms
+
+KeyPad_LCD_Service:
+        call    KeyPad_Read
+        movwf   key_char, A
+        movf    key_char, W, A
+        bz      svc_done
+
+        movlw   20
+        call    LCD_delay_ms
+
+        call    KeyPad_Read
+        movwf   key_char, A
+        movf    key_char, W, A
+        bz      svc_done
+
+        movlw   'C'
+        cpfseq  key_char, A
+        bra     svc_not_clear
+        call    LCD_clear_display
+        call    LCD_first_line
+        bra     svc_wait
+
+svc_not_clear:
+        movlw   'A'
+        cpfseq  key_char, A
+        bra     svc_not_left
+        call    LCD_shift_left_crsr
+        bra     svc_wait
+
+svc_not_left:
+        movlw   'B'
+        cpfseq  key_char, A
+        bra     svc_not_right
+        call    LCD_shift_right_crsr
+        bra     svc_wait
+
+svc_not_right:
+        movlw   'F'
+        cpfseq  key_char, A
+        bra     svc_not_first
+        call    LCD_first_line
+        bra     svc_wait
+
+svc_not_first:
+        movlw   'E'
+        cpfseq  key_char, A
+        bra     svc_not_second
+        call    LCD_second_line
+        bra     svc_wait
+
+svc_not_second:
+        movlw   'D'
+        cpfseq  key_char, A
+        bra     svc_not_delete
+        call    LCD_shift_left_crsr
+        movlw   ' '
+        call    LCD_Write_Char
+        call    LCD_shift_left_crsr
+        bra     svc_wait
+
+svc_not_delete:
+        movf    key_char, W, A
+        call    LCD_Write_Char
+
+svc_wait:
+        call    KeyPad_Read
+        movwf   key_char, A
+        movf    key_char, W, A
+        bnz     svc_wait
+
+        movlw   20
+        call    LCD_delay_ms
+
+svc_done:
+        return
